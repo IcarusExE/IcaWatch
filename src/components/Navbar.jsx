670b39afcacbe220ccafb5eb167127f7b7c37de5
Icarus <116387557+IcarusExE@users.noreply.github.com>
2023-10-19 00:05:43 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -84,7 +84,7 @@ function Navbar() {
                         :
                         <>
                             <Link to="/login" className="w-full bg-gray-900 py-2 gap-4 rounded-xl flex items-center justify-center font-semibold border-2 border-blue-100/10" onClick={() => setActivemobile(!activemobile)}>
-                                <h1>Log in</h1>
+                                <h1>Login</h1>
                             </Link>
                         </>
                     }
